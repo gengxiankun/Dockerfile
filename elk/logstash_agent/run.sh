ba8d220d9a811c43ad156179a4b3737c9411ad6e
xiankun.geng <ge2x3k2@gmail.com>
2017-05-22 14:03:52 +0800

    elk docker-compose.yml

--- elk/logstash_agent/run.sh
+++ elk/logstash_agent/run.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
+#!/bin/bash -x
 # auther: ge2x3k2@gmail.com
+PS4='+[$LINENO]'
 
 if [[ ! -z "${NGINX_PATH}" ]]; then
-	echo "info: configure the nginx path"
-	sed -i "s/\$NGINX_PATH/$NGINX_PATH/g" /etc/logstash/conf.d/logstash_agent.conf
+	sed -i "s/\$NGINX_PATH/$NGINX_PATH/g" /etc/logstash/conf.d/logstash_agent.conf 
 fi
 
-if [[ ! -z "${REDIS_HOST}" ]]; then
-	echo "info: configure redis host"
-	sed -i "s/\$REDIS_HOST/$REDIS_HOST/g" /etc/logstash/conf.d/logstash_agent.conf
+if [[ ! -z "${REDIS_HOST}" ]]; then 
+	sed -i "s/\$REDIS_HOST/$REDIS_HOST/g" /etc/logstash/conf.d/logstash_agent.conf 
 fi
 
 /usr/share/logstash/bin/logstash -f /etc/logstash/conf.d/logstash_agent.conf
--- elk/logstash_indexer/run.sh
+++ elk/logstash_indexer/run.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
+#!/bin/bash -x
 # auther: ge2x3k2@gmail.com
+PS4='+[$LINENO]'
 
-if [[ ! -z "${REDIS_HOST}" ]]; then
-	echo "info: configure redis host"
-	sed -i "s/\$REDIS_HOST/$REDIS_HOST/g" /etc/logstash/conf.d/logstash_indexer.conf
+if [[ ! -z "${REDIS_HOST}" ]]; then 
+	sed -i "s/\$REDIS_HOST/$REDIS_HOST/g" /etc/logstash/conf.d/logstash_indexer.conf 
 fi
 
-if [[ ! -z "${ELASTICSEARCH_HOST}" ]]; then
-	echo "info: configure elasticsearch host"
-	sed -i "s/\$ELASTICSEARCH_HOST/$ELASTICSEARCH_HOST/g" /etc/logstash/conf.d/logstash_indexer.conf
+if [[ ! -z "${ELASTICSEARCH_HOST}" ]]; then 
+	sed -i "s/\$ELASTICSEARCH_HOST/$ELASTICSEARCH_HOST/g" /etc/logstash/conf.d/logstash_indexer.conf 
 fi
 
 /usr/share/logstash/bin/logstash -f /etc/logstash/conf.d/logstash_indexer.conf
